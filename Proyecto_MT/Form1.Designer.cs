e5265a4a0be1fa60e830b43eb8614c72e2aaf90c
Luis Andrino <landrinomenendez@gmail.com>
2018-04-30 23:26:39 -0600

    ya salio la suma uniaria !!!!!!

--- Proyecto_MT/Form1.Designer.cs
+++ Proyecto_MT/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.txtCadena = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.RbtnPalindromo = new System.Windows.Forms.RadioButton();
+            this.RbtnCopiar = new System.Windows.Forms.RadioButton();
+            this.RbtnMultiplicacion = new System.Windows.Forms.RadioButton();
+            this.RbtnSuma = new System.Windows.Forms.RadioButton();
+            this.RbtnResta = new System.Windows.Forms.RadioButton();
             this.CintaMT = new System.Windows.Forms.DataGridView();
             this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblAviso = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.CintaMT)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,60 +57,60 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingrese la cadena que desee valuar ";
             // 
-            // radioButton1
+            // RbtnPalindromo
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 102);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.RbtnPalindromo.AutoSize = true;
+            this.RbtnPalindromo.Location = new System.Drawing.Point(12, 102);
+            this.RbtnPalindromo.Name = "RbtnPalindromo";
+            this.RbtnPalindromo.Size = new System.Drawing.Size(77, 17);
+            this.RbtnPalindromo.TabIndex = 2;
+            this.RbtnPalindromo.TabStop = true;
+            this.RbtnPalindromo.Text = "Palindromo";
+            this.RbtnPalindromo.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // RbtnCopiar
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(12, 126);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.RbtnCopiar.AutoSize = true;
+            this.RbtnCopiar.Location = new System.Drawing.Point(12, 126);
+            this.RbtnCopiar.Name = "RbtnCopiar";
+            this.RbtnCopiar.Size = new System.Drawing.Size(112, 17);
+            this.RbtnCopiar.TabIndex = 3;
+            this.RbtnCopiar.TabStop = true;
+            this.RbtnCopiar.Text = "Copia de Patrones";
+            this.RbtnCopiar.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // RbtnMultiplicacion
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(12, 150);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
-            this.radioButton3.TabIndex = 4;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.RbtnMultiplicacion.AutoSize = true;
+            this.RbtnMultiplicacion.Location = new System.Drawing.Point(12, 150);
+            this.RbtnMultiplicacion.Name = "RbtnMultiplicacion";
+            this.RbtnMultiplicacion.Size = new System.Drawing.Size(89, 17);
+            this.RbtnMultiplicacion.TabIndex = 4;
+            this.RbtnMultiplicacion.TabStop = true;
+            this.RbtnMultiplicacion.Text = "Multiplicación";
+            this.RbtnMultiplicacion.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // RbtnSuma
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(12, 174);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(85, 17);
-            this.radioButton4.TabIndex = 5;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.RbtnSuma.AutoSize = true;
+            this.RbtnSuma.Location = new System.Drawing.Point(12, 174);
+            this.RbtnSuma.Name = "RbtnSuma";
+            this.RbtnSuma.Size = new System.Drawing.Size(86, 17);
+            this.RbtnSuma.TabIndex = 5;
+            this.RbtnSuma.TabStop = true;
+            this.RbtnSuma.Text = "Suma Unaria";
+            this.RbtnSuma.UseVisualStyleBackColor = true;
             // 
-            // radioButton5
+            // RbtnResta
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(12, 198);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(85, 17);
-            this.radioButton5.TabIndex = 6;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "radioButton5";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.RbtnResta.AutoSize = true;
+            this.RbtnResta.Location = new System.Drawing.Point(12, 198);
+            this.RbtnResta.Name = "RbtnResta";
+            this.RbtnResta.Size = new System.Drawing.Size(87, 17);
+            this.RbtnResta.TabIndex = 6;
+            this.RbtnResta.TabStop = true;
+            this.RbtnResta.Text = "Resta Unaria";
+            this.RbtnResta.UseVisualStyleBackColor = true;
             // 
             // CintaMT
             // 
@@ -127,22 +128,32 @@
             this.btnIniciar.Name = "btnIniciar";
             this.btnIniciar.Size = new System.Drawing.Size(160, 23);
             this.btnIniciar.TabIndex = 8;
-            this.btnIniciar.Text = "Iniciar Maquina ";
+            this.btnIniciar.Text = "Go!";
             this.btnIniciar.UseVisualStyleBackColor = true;
             this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblAviso
+            // 
+            this.lblAviso.AutoSize = true;
+            this.lblAviso.Location = new System.Drawing.Point(326, 150);
+            this.lblAviso.Name = "lblAviso";
+            this.lblAviso.Size = new System.Drawing.Size(35, 13);
+            this.lblAviso.TabIndex = 9;
+            this.lblAviso.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(677, 244);
+            this.Controls.Add(this.lblAviso);
             this.Controls.Add(this.btnIniciar);
             this.Controls.Add(this.CintaMT);
-            this.Controls.Add(this.radioButton5);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.RbtnResta);
+            this.Controls.Add(this.RbtnSuma);
+            this.Controls.Add(this.RbtnMultiplicacion);
+            this.Controls.Add(this.RbtnCopiar);
+            this.Controls.Add(this.RbtnPalindromo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCadena);
             this.Name = "Form1";
@@ -157,13 +168,14 @@
 
         private System.Windows.Forms.TextBox txtCadena;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton RbtnPalindromo;
+        private System.Windows.Forms.RadioButton RbtnCopiar;
+        private System.Windows.Forms.RadioButton RbtnMultiplicacion;
+        private System.Windows.Forms.RadioButton RbtnSuma;
+        private System.Windows.Forms.RadioButton RbtnResta;
         private System.Windows.Forms.DataGridView CintaMT;
         private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblAviso;
     }
 }
 
--- Proyecto_MT/Form1.cs
+++ Proyecto_MT/Form1.cs
@@ -12,28 +12,172 @@ namespace Proyecto_MT
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+        int contador = 0;
+        string cadena;
+        string[] datos;
+        string aux;
+        int cabezal = 0;
+        int auxCabezal;
+        bool bandera = false;
+        bool escribir = false;
+
+    public Form1()
         {
             InitializeComponent();
         }
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
-            string cadena = txtCadena.Text+"="+"B";
-            string[] datos = new string[cadena.Length];
-            
-            for (int i = 0; i < cadena.Length; i++)
+        
+
+            if (RbtnPalindromo.Checked)
             {
-                datos[i] = cadena[i].ToString();
-                CintaMT.Columns.Add(i.ToString(), i.ToString());
-                CintaMT[i, 0].Value = datos[i];
-               
-          
+                
+             //q0
+                if (contador == 0)
+                {
+                    cadena = txtCadena.Text + "B";
+                    datos = new string[cadena.Length];
+                    cabezal = 0;
+
+                    for (int i = 0; i < cadena.Length; i++)
+                    {
+                        datos[i] = cadena[i].ToString();
+                        CintaMT.Columns.Add(i.ToString(), i.ToString());
+                        CintaMT[i, 0].Value = datos[i];
+                    }
+                    aux = datos[cabezal];
+                    if (aux == "a")
+                    {
+                        CintaMT.Rows[0].Cells[cabezal].Value = "B";
+                        CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.Yellow;
+                        
+
+                    }
+                } 
+                //estado 1 
+                if (contador == 1)
+                {
+                    for (int i = 1 ; i < datos.Length; i++)
+                    {
+                        auxCabezal = i;
+                        if (datos[i] == "B")
+                        {
+                            CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.White;
+                            CintaMT.Rows[0].Cells[auxCabezal].Style.BackColor = Color.Yellow;
+                        }
+                    }
+
+                }
+                contador++;
+            }
+            if (RbtnCopiar.Checked)
+            {
 
             }
-            
+            if (RbtnMultiplicacion.Checked)
+            {
+
+            }
+            if (RbtnSuma.Checked)
+            {
+                switch (contador)
+                {
+                    case (0):
+                        cadena = txtCadena.Text;
+                        datos = new string[cadena.Length];
+
+
+                        for (int i = 0; i < cadena.Length; i++)
+                        {
+                            datos[i] = cadena[i].ToString();
+                            CintaMT.Columns.Add(i.ToString(), i.ToString());
+                            CintaMT[i, 0].Value = datos[i];
+                        }
+
+
+
+
+                        aux = datos[cabezal];
+                        if (aux == "1")
+                        {
+                            CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.Yellow;
+                            cabezal++;
+                            if (escribir)
+                            {
+                                int a = cabezal - 1;
+                                CintaMT.Rows[0].Cells[a - 1].Style.BackColor = Color.White;
+                            }
+                            escribir = true;
+
+                        }
+
+                        else if (aux == "+")
+                        {
+                            CintaMT.Rows[0].Cells[cabezal - 1].Style.BackColor = Color.White;
+                            CintaMT.Rows[0].Cells[cabezal].Value = "B";
+                            CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.Yellow;
+                            contador++;
+                            cabezal++;
+                            
+
+                        }
+                        break;
+                    case (1):
+                        if (cabezal >= cadena.Length)
+                        {
+                            lblAviso.Text = "TERMINIOOOOO!!!!";
+                            break;
+                        }
+
+                            aux = datos[cabezal];
+                            if (aux == "1")
+                            {
+                                CintaMT.Rows[0].Cells[cabezal - 1].Style.BackColor = Color.White;
+                                CintaMT.Rows[0].Cells[cabezal].Value = "B";
+                                CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.Yellow;  
+                                cabezal--;
+                                contador++;
+                            }
+                            
+
+                   
+                        break;
+                    case (2):
+                        if (CintaMT.Rows[0].Cells[cabezal].Value.ToString() == "B")
+                        {
+
+                                CintaMT.Rows[0].Cells[cabezal + 1].Style.BackColor = Color.White;
+                                CintaMT.Rows[0].Cells[cabezal].Value = "1";
+                                CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.Yellow;
+                                cabezal++;
+                                contador++;
+                         }
+
+                        
+                        break;
+                    case (3):
+                        if (CintaMT.Rows[0].Cells[cabezal].Value.ToString() == "B")
+                        {
+                            CintaMT.Rows[0].Cells[cabezal - 1].Style.BackColor = Color.White;
+                            CintaMT.Rows[0].Cells[cabezal].Style.BackColor = Color.Yellow;
+                            cabezal++;
+                            contador = 1;
+
+                        }
+
+                            break;
+                }
+                
+            }
+            if (RbtnResta.Checked)
+            {
+
+            }
 
 
         }
+
     }
 }
